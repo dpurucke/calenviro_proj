bfe55d89035661985789151262a34657e777b465
David Purucker <davidpurucker@yahoo.com>
2020-10-27 17:12:17 -0700

    code to load my cleaned dataset into the models script, some initial code constructing linear regression models and viewing their output

--- analysis/models_calenviro.R
+++ analysis/models_calenviro.R
@@ -7,5 +7,14 @@
 # test the strength of associations between various measures of environmental pollution burden 
 # on the incidence of low birth-weight in California census tracts.
 
+# Set working directory
+setwd("~/Desktop/sociology/soc_year2/Soc 412:512 (Stats II)/calenviro_proj")
+
+# Load cleaned dataset
+load("~/Desktop/sociology/soc_year2/Soc 412:512 (Stats II)/calenviro_proj/output/ces3_complete.RData")
+
 # Construct exploratory models
-model1 <- lm_svy_mi()
+model1 <- lm(lbw~pm2.5+dieselpm, data=ces3_complete)
+
+# Analyze models
+summary(model1)
